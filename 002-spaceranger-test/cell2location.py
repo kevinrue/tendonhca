32952bb54e371a0bbb1f123e79543ef098bf5f3a
Kevin Rue-Albrecht <kevinrue@users.noreply.github.com>
2023-05-09 12:11:36 +0100

    adjust script to work in this situation

--- 002-spaceranger-test/cell2location.py
+++ 002-spaceranger-test/cell2location.py
@@ -27,18 +27,18 @@ warnings.filterwarnings('ignore')
 ###
 
 # Set paths to data and results used through the document:
-sp_data_folder = './OMB1556_Ach_Enth_H/'
+sp_data_folder = './'
 
 ###
 
 def read_and_qc(sample_name, path=sp_data_folder):
     r""" This function reads the data for one 10X spatial experiment into the anndata object.
     It also calculates QC metrics. Modify this function if required by your workflow.
-
+    
     :param sample_name: Name of the sample
     :param path: path to data
     """
-
+    
     adata = sc.read_visium(path + str(sample_name),
                            count_file='filtered_feature_bc_matrix.h5', load_images=True)
     adata.obs['sample'] = sample_name
@@ -46,7 +46,7 @@ def read_and_qc(sample_name, path=sp_data_folder):
     adata.var.rename(columns={'gene_ids': 'ENSEMBL'}, inplace=True)
     adata.var_names = adata.var['ENSEMBL']
     adata.var.drop(columns='ENSEMBL', inplace=True)
-
+    
     # Calculate QC metrics
     from scipy.sparse import csr_matrix
     adata.X = adata.X.toarray()
@@ -54,13 +54,13 @@ def read_and_qc(sample_name, path=sp_data_folder):
     adata.X = csr_matrix(adata.X)
     adata.var['mt'] = [gene.startswith('mt-') for gene in adata.var['SYMBOL']]
     adata.obs['mt_frac'] = adata[:, adata.var['mt'].tolist()].X.sum(1).A.squeeze()/adata.obs['total_counts']
-
+    
     # add sample name to obs names
     adata.obs["sample"] = [str(i) for i in adata.obs['sample']]
     adata.obs_names = adata.obs["sample"] \
                           + '_' + adata.obs_names
     adata.obs.index.name = 'spot_id'
-
+    
     return adata
 
 def select_slide(adata, s, s_col='sample'):
@@ -70,23 +70,23 @@ def select_slide(adata, s, s_col='sample'):
     :param s: name of selected experiment
     :param s_col: column in adata.obs listing experiment name for each location
     """
-
+    
     slide = adata[adata.obs[s_col].isin([s]), :]
     s_keys = list(slide.uns['spatial'].keys())
     s_spatial = np.array(s_keys)[[s in k for k in s_keys]][0]
-
+    
     slide.uns['spatial'] = {s_spatial: slide.uns['spatial'][s_spatial]}
-
+    
     return slide
 
 #######################
 # Read the list of spatial experiments
-sample_data = pd.read_csv(sp_data_folder + 'Visium_samples.csv')
+sample_data = pd.read_csv('Visium_samples.csv')
 
 # Read the data into anndata objects
 slides = []
 for i in sample_data['sample_name']:
-    slides.append(read_and_qc(i, path=sp_data_folder + 'rawdata/'))
+    slides.append(read_and_qc(i, path=sp_data_folder))
 
 # Combine anndata objects together
 adata = slides[0].concatenate(
