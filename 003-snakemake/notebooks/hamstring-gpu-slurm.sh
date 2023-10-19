9ba63a9cbd83d232df68d14021d161e9b35143dd
Kevin Rue-Albrecht <kevinrue@users.noreply.github.com>
2023-10-19 13:50:07 +0100

    try loading module

--- 003-snakemake/notebooks/hamstring-gpu-slurm.sh
+++ 003-snakemake/notebooks/hamstring-gpu-slurm.sh
@@ -48,5 +48,7 @@ cd /ceph/project/tendonhca/albrecht/003-snakemake/
 eval "$(/project/sims-lab/albrecht/miniforge3/bin/conda shell.bash hook)" && conda activate base
 conda activate envs/cell2location-nb
 
+module load cuda/12.2
+
 PYTORCH_ENABLE_MPS_FALLBACK=1 jupyter nbconvert --to notebook --allow-errors --execute notebooks/hamstring-gpu.ipynb
 #jupyter nbconvert --to notebook --inplace --allow-errors --execute notebooks/hamstring-gpu.ipynb
