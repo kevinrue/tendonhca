8ad9f1355476b5a574ad98ef80dc112d27696c5b
Kevin Rue-Albrecht <kevinrue@users.noreply.github.com>
2023-10-20 16:14:30 +0100

    print messages

--- 003-snakemake/scripts/seurat_transfer_data.R
+++ 003-snakemake/scripts/seurat_transfer_data.R
@@ -13,6 +13,7 @@ sample_name <- basename(dirname(spaceranger_out_dir))
 reference_rds <- "/ceph/project/tendonhca/albrecht/003-snakemake/data/HAMSTRING_singlets_ambRNA0.2_res0.15.RDS"
 
 # === Outputs ===
+message("=== Process outputs ===")
 predictions_png <- snakemake@output[["predictions_png"]]
 out_dir <- dirname(predictions_png)
 
@@ -20,25 +21,26 @@ out_dir <- dirname(predictions_png)
 use_pcs <- 15
 use_resolution <- 0.3
 
-# Load the slide as a Seurat object
+message("=== Load the slide as a Seurat object ===")
 seurat_slide <- Load10X_Spatial(spaceranger_out_dir)
 seurat_slide
 
-# Remove spots with fewer than 50 UMI
+message("=== Remove spots with fewer than 50 UMI ===")
 seurat_slide <- subset(seurat_slide, nCount_Spatial > 50)
 seurat_slide
 
-# Run SCT on the slide data
+message("=== Run SCT on the slide data ===")
 seurat_slide <- SCTransform(seurat_slide, assay = "Spatial", verbose = TRUE)
 seurat_slide
 
 # Run standard workflow
+message("=== Run standard workflow ===")
 seurat_slide <- RunPCA(seurat_slide, assay = "SCT", verbose = FALSE)
 seurat_slide <- FindNeighbors(seurat_slide, reduction = "pca", dims = 1:use_pcs)
 seurat_slide <- FindClusters(seurat_slide, resolution = use_resolution, verbose = FALSE)
 seurat_slide <- RunUMAP(seurat_slide, reduction = "pca", dims = 1:use_pcs)
 
-# Plot clusters in spatial layout
+message("=== Plot clusters in spatial layout ===")
 p1 <- DimPlot(seurat_slide, reduction = "umap", label = TRUE)
 p2 <- SpatialDimPlot(seurat_slide, label = TRUE, label.size = 3, alpha = c(0.5, 0.5))
 ggsave(
@@ -48,11 +50,11 @@ ggsave(
   height = 6
 )
 
-# Load reference data
+message("=== Load reference data ===")
 reference <- readRDS(reference_rds)
 reference
 
-# Load and preprocess feature mapping table
+message("=== Load and preprocess feature mapping table ===")
 ## NOTE: reference uses ENSEMBL while slide uses SYMBOL
 sample_features_tsv <- sprintf(
   "results/spaceranger_count/%s/outs/filtered_feature_bc_matrix/features.tsv.gz",
@@ -64,8 +66,7 @@ features_tsv <- features_tsv[, 1:2]
 colnames(features_tsv) <- c("ENSEMBL", "SYMBOL")
 rownames(features_tsv) <- features_tsv$ENSEMBL
 
-# Make a new Seurat object after renaming ENSEMBL to SYMBOL
-
+message("=== Make a new Seurat object after renaming ENSEMBL to SYMBOL ===")
 reference_counts <- reference@assays$RNA@counts
 reference_counts <- reference_counts[intersect(rownames(reference_counts), features_tsv$ENSEMBL), ]
 rownames(reference_counts) <- features_tsv[rownames(reference_counts), "SYMBOL"]
@@ -77,13 +78,14 @@ levels(reference_label)[c(1, 2)] <- c("Muscle cells", "Fibroblasts")
 reference_symbols$label <- reference_label
 Idents(reference_symbols) <- "cell_type"
 
-# Transfer data (make predictions)
+message("=== Transfer data (make predictions) ===")
 anchors <- FindTransferAnchors(reference = reference_symbols, query = seurat_slide, normalization.method = "SCT")
 predictions.assay <- TransferData(anchorset = anchors, refdata = Idents(reference_symbols),
                                   prediction.assay = TRUE,
                                   weight.reduction = seurat_slide[["pca"]], dims = 1:30)
 seurat_slide[["predictions"]] <- predictions.assay
 
+message("=== Save predictions to file ===")
 DefaultAssay(seurat_slide) <- "predictions"
 p <- SpatialFeaturePlot(
   seurat_slide,
